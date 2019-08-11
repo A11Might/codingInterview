e15eb46483a5591523e33ca7e6b6eab6716359ec
Qihang Hu <qihanghu@foxmail.com>
2019-08-11 16:37:22 +0800

    2019.8.11

---
+++ code/offer09.java
@@ -0,0 +1,40 @@
+package offer;
+
+import java.util.Stack;
+
+/**
+ * [9] 用两个栈实现队列
+ * 
+ * 题目：用两个栈来实现一个队列
+ * 
+ * 思路：使用两个辅助栈，一个输入栈一个输出栈
+ */
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        // node always push in stack1
+        stack1.push(node);
+        // when stack2 is empty
+        // push all stack1's element to stack2
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+    }
+    
+    public int pop() {
+        // pop operate always pop stack2
+        int res = stack2.pop();
+        // when stack2 is empty
+        // push all stack1's element to stack2
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return res;
+    }
+}
