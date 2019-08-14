90eca5fdd048dcc5e7f1a5504e53d1dd61cf66d1
Qihang Hu <qihanghu@foxmail.com>
2019-08-14 14:55:11 +0800

    Create offer322.java

---
+++ code/offer322.java
@@ -0,0 +1,59 @@
+package offer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+/**
+ * [32] 分行从上到下打印二叉树
+ * 
+ * 题目：从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行
+ * 
+ * 思路：BFS
+ */
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (pRoot == null) {
+            return res;
+        }
+        
+        return bfs(pRoot, res);
+    }
+
+    ArrayList<ArrayList<Integer>> bfs(TreeNode node, ArrayList<ArrayList<Integer>> res) {
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> subList = new ArrayList<>();
+            int curLevelNum = queue.size();
+            for (int i = 0; i < curLevelNum; i++) {
+                TreeNode cur = queue.poll();
+                subList.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(subList);
+        }
+        
+        return res;
+    }
+    
+}
